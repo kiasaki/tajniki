f4c48263f8f2b38d4e93a3389b6d2688f0e210e6
kiasaki <a@4ki.me>
2026-08-10 23:01:41 -0400

    load error

--- tajniki.go
+++ tajniki.go
@@ -1,12 +1,29 @@
 // Package tajniki loads encrypted secrets from a JSON file.
 package tajniki
 
-import "github.com/kiasaki/tajniki/internal/secrets"
+import (
+	"log"
+	"os"
+
+	"github.com/kiasaki/tajniki/internal/secrets"
+)
 
 // Load reads and decrypts the configured secrets file.
 //
 // TAJNIKI_FILE sets the file path. The default is secrets/local.json.
 // TAJNIKI_SECRET sets the password.
-func Load() (map[string]string, error) {
+func Read() (map[string]string, error) {
 	return secrets.LoadFromEnvironment()
+}
+
+func Load() {
+	secrets, err := secrets.LoadFromEnvironment()
+	if err != nil {
+		log.Printf("tajniki: error loading secrets: %v", err)
+		os.Exit(1)
+		return
+	}
+	for k, v := range secrets {
+		os.Setenv(k, v)
+	}
 }
